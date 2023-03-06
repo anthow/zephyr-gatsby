f763a00bd5ae756c84667caeefce37905499425d
anthow <anthonyenglebert@gmail.com>
2023-03-06 23:11:00 +0100

    Update layout.js

--- src/component/layout.js
+++ src/component/layout.js
@@ -8,10 +8,8 @@ const Layout = ({ children }) => {
         
     <>
     <Header />
-    <div className=" mt-10 min-h-screen flex flex-col h-screen">
-    <section className=" fullheader w-12/12 m-auto fixed bg-white z-50 font-sans">
-      </section>
-      <section className="w-12/12 mt-24 md:mt-0  m-auto font-sans">
+    <div className=" min-h-screen flex flex-col h-screen">
+      <section className="w-12/12  md:mt-0  m-auto font-sans">
         <main className="flex-1 md:mt-52">{children}</main>
        </section>
      
