794535098a940fd259e91ae40e4b30bc3c75db39
anthow <anthonyenglebert@gmail.com>
2025-03-20 10:08:31 +0100

    Update asbl.js

--- src/pages/asbl.js
+++ src/pages/asbl.js
@@ -2,130 +2,97 @@ import * as React from "react";
 import Layout from "../component/layout";
 import { GatsbyImage } from "gatsby-plugin-image";
 import { graphql } from "gatsby";
-import Seo from "../component/seo";
+import ReactAudioPlayer from "react-audio-player";
 import SliderFooter from "../component/slider/slider-footer";
 
-const asblPage = ({ data }) => {
-  return (
-    <Layout>
-        <Seo
-          title={data.datoCmsPageAsbl.seo.title}
-          description={data.datoCmsPageAsbl.seo.description}
+const temoignagesPage = ({ data }) => (
+  <Layout>
+    <div className="flex flex-col gap-10 md:gap-20">
+      <article className="w-12/12">
+        {/* Validation si imageTMoignage existe avant de tenter de l'afficher */}
+        {data.datoCmsPageTemoignage?.imageTMoignage?.gatsbyImageData && (
+          <figure>
+            <GatsbyImage
+              className="w-full"
+              image={data.datoCmsPageTemoignage.imageTMoignage.gatsbyImageData}
+              alt={data.datoCmsPageTemoignage.imageTMoignage.alt || "Image sans description"}
+            />
+          </figure>
+        )}
+      </article>
+      <article className="m-auto flex flex-col w-10/12 m-auto text-center gap-10">
+        <h1 className="text-2xl text-bleu-z font-semibold">
+          {data.datoCmsPageTemoignage?.titreDeLaPage}
+        </h1>
+        <div
+          className="flex flex-col gap-5"
+          dangerouslySetInnerHTML={{
+            __html: data.datoCmsPageTemoignage?.textePage || "",
+          }}
         />
-      <main className="flex flex-col gap-20 mb-20">
-        <article className="w-12/12 h-1/2">
-          <figure>
-            <GatsbyImage
-              className=" m-auto max-w-full"
-              image={data.datoCmsPageAsbl.immageAsbl.gatsbyImageData}
-              alt={data.datoCmsPageAsbl.immageAsbl.alt}
-            />
-          </figure>
-        </article>
-        <article className="m-auto w-10/12 flex flex-col gap-10">
-          <h1 className="text-3xl text-bleu-z font-semibold">
-            {data.datoCmsPageAsbl.titreIntroduction}
-          </h1>
-
-          <div
-            className="flex flex-col gap-5"
-            dangerouslySetInnerHTML={{
-              __html: data.datoCmsPageAsbl.texteIntroduction,
-            }}
-          />
-        </article>
-        <article className="flex flex-col gap-10 w-10/12 m-auto">
-          <h2 className="text-2xl text-bleu-z text-center font-semibold">
-            Notre Équipe
-          </h2>
-          <section className="flex flex-col md:grid grid-cols-3 m-auto gap-y-10">
-            {data.allDatoCmsListeEquipe.edges.map(({ node }) => {
-              return (
-                <div className="flex flex-col gap-5 text-center place-self-strech" key={node.id}>
-                  <figure>
-                    <GatsbyImage
-                      className="rounded-full"
-                      image={node.imageDeLaPersonne.gatsbyImageData}
-                      alt={node.imageDeLaPersonne.alt}
-                    />
-                  </figure>
-                  <div className="flex flex-col gap-2">
-                    <h3 className="text-lg text-orange-z ">
-                      {node.nomEtPrenomDeLaPersonne}
-                    </h3>
-                    <p className="text-m font-semibold">{node.fonction}</p>
-                    <p className="underline">{node.adresseMail}</p>
-                    <p>{node.numeroDeTelephone}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </section>
-        </article>
-        <article className="m-auto w-10/12 flex flex-col md:grid grid-cols-2 gap-10 md:gap-20">
-          <figure>
-            <GatsbyImage
-              className=" m-auto max-w-full"
-              image={data.datoCmsPageAsbl.photoConseil.gatsbyImageData}
-              alt={data.datoCmsPageAsbl.photoConseil.alt}
-            />
-          </figure>
-          <section className="flex flex-col gap-5 gap-10">
-            <h2 className="text-2xl text-bleu-z font-semibold ">
-              {data.datoCmsPageAsbl.titreConseil}
-            </h2>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: data.datoCmsPageAsbl.texteConseil,
-              }}
-            />
-          </section>
-        </article>
-        <SliderFooter />
-      </main>
-    </Layout>
-  );
-};
+        <section className="flex flex-col md:grid grid-cols-3 items-center gap-20">
+          {data.allDatoCmsListeTMoignage.edges.map(({ node }) => (
+            <div className="flex flex-col p-5 gap-10" key={node.id}>
+              <div className="flex flex-row gap-10 justify-between items-center">
+                {/* Validation si enregistrement.url existe avant d'afficher le lecteur audio */}
+                {node.enregistrement?.url && (
+                  <ReactAudioPlayer src={node.enregistrement.url} controls />
+                )}
+              </div>
+              <div className="flex gap-5 items-center flex-row">
+                {/* Validation si drapeau existe avant d'afficher l'image */}
+                {node.drapeau?.gatsbyImageData && (
+                  <GatsbyImage
+                    className=""
+                    image={node.drapeau.gatsbyImageData}
+                    alt={node.drapeau.alt || "Drapeau sans description"}
+                  />
+                )}
+                <p>{node.description}</p>
+              </div>
+            </div>
+          ))}
+        </section>
+      </article>
+    </div>
+    <SliderFooter />
+  </Layout>
+);
 
 export const query = graphql`
   query {
-    datoCmsPageAsbl {
+    allDatoCmsListeTMoignage(sort: { id: DESC }) {
+      edges {
+        node {
+          id
+          description
+          drapeau {
+            alt
+            gatsbyImageData(width: 50)
+          }
+          enregistrement {
+            url
+            alt
+          }
+        }
+      }
+    }
+    datoCmsPageTemoignage {
       seo {
         description
         title
         image {
-          gatsbyImageData
+          gatsbyImageData(aspectRatio: 3, height: 400)
         }
       }
-      immageAsbl {
-        alt
-        gatsbyImageData(height: 400, width: 1600)
-      }
-      texteConseil
-      texteIntroduction
-      titreConseil
-      titreIntroduction
-      photoConseil {
+      imageTMoignage {
         alt
         gatsbyImageData
       }
-    }
-    allDatoCmsListeEquipe(sort: { ordre: ASC }) {
-      edges {
-        node {
-          id
-          adresseMail
-          fonction
-          imageDeLaPersonne {
-            alt
-            gatsbyImageData(width: 200, height: 200)
-          }
-          nomEtPrenomDeLaPersonne
-          numeroDeTelephone
-        }
-      }
+      textePage
+      titreDeLaPage
     }
   }
 `;
 
-export default asblPage;
+export default temoignagesPage;
